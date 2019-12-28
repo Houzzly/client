5875d40a127c459da870be8391097ec3aed847e5
Israel Aderogba <israeladerogba@gmail.com>
2019-12-28 15:57:48 +0000

    feat(client): initial design for footer

--- src/~reusables/layout/Footer.js
+++ src/~reusables/layout/Footer.js
@@ -1,18 +1,39 @@
 import React from 'react';
+
 import styled from 'styled-components';
+import { heading_4 } from '../variables/FontSizes';
 
 // TODO: Complete footer design
 const Footer = () => {
 	return (
 		<FooterContainer>
-			<h1>Footer</h1>
+			<div>
+				<p>
+					Made by <a href="https://github.com/IsraelAde">Israel Aderogba</a> &{' '}
+					<a href="https://github.com/IsaacAderogba">Isaac Aderogba</a>
+				</p>
+			</div>
 		</FooterContainer>
 	);
 };
 
 const FooterContainer = styled.div`
-	h1 {
-		font-size: 24px;
+	height: 10vh;
+	min-height: 60px;
+
+	div {
+		display: flex;
+		align-items: center;
+		margin: 0 auto;
+		max-width: 1280px;
+	}
+
+	p {
+		font-size: ${heading_4};
+	}
+
+	a {
+		text-decoration: none;
 	}
 `;
 
